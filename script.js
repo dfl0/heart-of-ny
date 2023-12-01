75e1d0e77fe7650daf758b2e60d98a28d8a48962
dfl0 <dflocos@gmail.com>
2023-11-30 21:19:30 -0500

    add zoom and pan limit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var map = L.map('map', {
-    center: [42.10442541105549, -75.91265528372267],
+    center: [42.1044, -75.9127],
     zoom: 8,
+    minZoom: 8,
+    maxBounds: L.latLngBounds([40.8821, -80.0110], [43.3267, -71.8335]),
+    maxBoundsViscosity: 1
 });
 
 

59e09f0af780fba66ad0b9e67ee079c2f7ac11e1
dfl0 <dflocos@gmail.com>
2023-12-20 21:41:58 -0500

    add county borders with hover effect

--- script.js
+++ script.js
@@ -6,12 +6,54 @@ var map = L.map('map', {
     maxBoundsViscosity: 1
 });
 
+var overlayStyle = {
+    "color": "#FFFFFF",
+    "weight": 2,
+    "opacity": 0.5,
+    "fillColor": "#FFFFFF",
+    "fillOpacity": 0
+};
+
+var hoveredStyle = {
+    "color": "#FF0000",
+    "weight": 2,
+    "opacity": 0,
+    "fillColor": "#FF0000",
+    "fillOpacity": 0.5
+};
+
+
+function onMouseOver(e) {
+    e.target.setStyle(hoveredStyle);
+}
+
+function onMouseOut(e) {
+    e.target.setStyle(overlayStyle);
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: onMouseOver,
+        mouseout: onMouseOut
+    });
+}
+
 
 function main() {
+    // add tile layer to map
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    // add county overlay layer
+    L.geoJSON(counties, {
+        style: overlayStyle,
+        onEachFeature: onEachFeature
+    }).addTo(map);
+
+    // add marker at TechWorks!
+    L.marker([42.10442541105549, -75.91265528372267]).addTo(map).bindPopup('TechWorks!').openPopup();
 }
 
 

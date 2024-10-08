d42a5209eecaa7e00a7f16ce993ae59dc93cc92c
Dorin Flocos <dflocos@gmail.com>
2024-10-08 16:53:26 -0400

    fetch the counties and societies from separate routes

---
+++ api/counties.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const router = express.Router();
+
+const corsOptions = {
+  origin: [process.env.URL]
+};
+
+router.use(cors(corsOptions));
+
+const pool = require("../db");
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query(`SELECT * FROM county`);
+    res.json(result.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
--- script.js
+++ script.js
@@ -23,51 +23,6 @@ let hoveredStyle = {
 };
 
 
-function onEachFeature(feature, layer) {
-  let name = L.tooltip({
-    content: "<p>" + feature.properties.name + "</p>",
-    className: "label",
-    direction: "center",
-    opacity: 1
-  });
-
-  layer.bindTooltip(name);
-
-  function showInfo() {
-    const container = document.getElementById("info-container");
-    container.setAttribute("show", "true");
-
-    const close = document.getElementById("info-close");
-    close.onclick = () => hideInfo();
-
-    // console.log(feature.properties.name)
-    const name = document.getElementById("info-name");
-    name.innerHTML = "<p>" + feature.properties.name + "</p>"
-  }
-
-  function hideInfo() {
-    const container = document.getElementById("info-container");
-    container.setAttribute("show", "false");
-  }
-
-  function onMouseOver(e) {
-    e.target.setStyle(hoveredStyle);
-  }
-
-  function onMouseOut(e) {
-    e.target.setStyle(overlayStyle);
-  }
-
-  function onMouseClick(layer) {
-    showInfo(layer);
-  }
-
-  layer.on({
-    mouseover: onMouseOver,
-    mouseout: onMouseOut,
-    click: onMouseClick
-  });
-}
 
 async function main() {
   // fetch the counties stored in Heart of NY database
@@ -76,8 +31,65 @@ async function main() {
     .then((data) => data)
     .catch(error => console.error('Error fetching data:', error));
 
+  // fetch the county societies stored in Heart of NY database
+  const DB_SOCIETIES = await fetch("http://localhost:3000/api/societies")
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch(error => console.error('Error fetching data:', error));
+
+
   // filter GeoJSON data to only include counties that are in the database
   const FILTERED_COUNTIES_GEOJSON = COUNTIES_GEOJSON.features.filter((feature) => DB_COUNTIES.map((county) => county.county_name) .includes(feature.properties.name));
+
+  function onEachFeature(feature, layer) {
+    let name = L.tooltip({
+      content: feature.properties.name,
+      className: "label",
+      direction: "center",
+      opacity: 1
+    });
+
+    layer.bindTooltip(name);
+
+    function showInfo() {
+      const container = document.getElementById("info-container");
+      container.setAttribute("show", "true");
+
+      const close = document.getElementById("info-close");
+      close.onclick = () => hideInfo();
+
+      const county = DB_COUNTIES.find((county) => county.county_name === feature.properties.name);
+      const society = DB_SOCIETIES.find((society) => society.county_id === county.county_id);
+
+      const infoName = document.getElementById("info-name");
+      const infoSociety = document.getElementById("info-society");
+      infoName.innerHTML = county.county_name;
+      infoSociety.innerHTML = `<a href=${society.website}>${society.society_name}</a>`;
+    }
+
+    function hideInfo() {
+      const container = document.getElementById("info-container");
+      container.setAttribute("show", "false");
+    }
+
+    function onMouseOver(e) {
+      e.target.setStyle(hoveredStyle);
+    }
+
+    function onMouseOut(e) {
+      e.target.setStyle(overlayStyle);
+    }
+
+    function onMouseClick(layer) {
+      showInfo(layer);
+    }
+
+    layer.on({
+      mouseover: onMouseOver,
+      mouseout: onMouseOut,
+      click: onMouseClick
+    });
+  }
 
   // add tile layer to map
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
